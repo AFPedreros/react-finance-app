e375baeb1ddbb5da8e4e4809158dc309d262223c
afpedreros <felipe.pedreros94@gmail.com>
2024-08-19 10:04:08 -0500

    refactor: Remove unused files and configurations

--- db/types/account.ts
+++
@@ -1,6 +0,0 @@
-import { z } from "zod";
-
-import { createAccountSchema, insertAccountsSchema } from "../schemas";
-
-export type Account = z.infer<typeof insertAccountsSchema>;
-export type CreateAccount = z.infer<typeof createAccountSchema>;
