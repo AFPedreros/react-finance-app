fe81a19a026480878ed9536df0dbdbba80c6c8f8
afpedreros <felipe.pedreros94@gmail.com>
2024-08-19 10:04:08 -0500

    refactor: Remove unused files and configurations

--- api/index.ts
+++
@@ -1,5 +0,0 @@
-import { serve } from "@hono/node-server";
-
-import app from "./[[...route]]";
-
-serve({ fetch: app.fetch, port: 3000 });
--- src/components/layouts/accounts-layout.tsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-export function AccountsLayout({ children }: { children: React.ReactNode }) {
-  return (
-    <section className="flex flex-col items-start justify-center gap-4 md:flex-row">
-      {children}
-    </section>
-  );
-}
