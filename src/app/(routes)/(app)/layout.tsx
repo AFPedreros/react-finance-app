2e606f70ecc7836d2b7caad50942ce21bf2ef88a
afpedreros <felipe.pedreros94@gmail.com>
2024-08-25 15:06:13 -0500

    feat: Add SidebarLayout component for improved layout management

--- src/app/(routes)/(app)/layout.tsx
+++ src/app/(routes)/(app)/layout.tsx
@@ -1,33 +1,22 @@
-"use client";
-
-import { cn } from "@nextui-org/theme";
+import { cookies } from "next/headers";
 import { ReactNode } from "react";
-import { useLocalStorage, useMediaQuery } from "usehooks-ts";
 
-import { Sidebar } from "@/components/sidebar";
+import { SidebarLayout } from "@/components/sidebar-layout";
 
 export default function AppLayout({
   children,
 }: Readonly<{
   children: ReactNode;
 }>) {
-  const [isCollapsed, setIsCollapsed] = useLocalStorage("isCollapsed", false);
-  const isMobile = useMediaQuery("(max-width: 768px)");
-
-  const isCompact = isCollapsed || isMobile;
+  const cookieStore = cookies();
+  const isCollapsedCookie = cookieStore.get("isCollapsed");
+  const defaultIsCollapsed = isCollapsedCookie
+    ? JSON.parse(isCollapsedCookie.value)
+    : false;
 
   return (
-    <div className="flex w-full">
-      <Sidebar isCompact={isCompact} setIsCollapsed={setIsCollapsed} />
-
-      <div
-        className={cn(
-          "w-full flex-1 pl-16 transition-all duration-300 md:pl-72",
-          { "md:pl-16": isCompact },
-        )}
-      >
-        {children}
-      </div>
-    </div>
+    <SidebarLayout defaultIsCollapsed={defaultIsCollapsed}>
+      {children}
+    </SidebarLayout>
   );
 }
