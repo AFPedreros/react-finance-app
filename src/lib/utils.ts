286b1cbc3c1e3449d712a242850ecab9ceab67d4
afpedreros <felipe.pedreros94@gmail.com>
2024-08-27 07:23:43 -0500

    feat: add Drizzle to the project

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,11 +1,13 @@
 import { isServer } from "@tanstack/react-query";
 
+import { env } from "@/config/env";
+
 export function getBaseURL() {
   if (!isServer) {
     return "";
   }
-  if (process.env.VERCEL_URL) {
-    return `https://${process.env.VERCEL_URL}`;
+  if (env.VERCEL_URL) {
+    return `https://${env.VERCEL_URL}`;
   }
 
   return "http://localhost:3000";
