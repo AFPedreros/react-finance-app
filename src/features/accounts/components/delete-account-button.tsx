06171de237b06db1aff62b7826f1e799baf4124c
afpedreros <felipe.pedreros94@gmail.com>
2024-09-04 09:24:51 -0500

    fix: getTotalBalanceAccountsQueryOptions errors

--- src/features/accounts/components/delete-account-button.tsx
+++ src/features/accounts/components/delete-account-button.tsx
@@ -33,7 +33,7 @@ export const DeleteAccountButton = ({
       );
 
       queryClient.setQueryData(
-        getTotalBalanceAccountsQueryOptions.queryKey,
+        getTotalBalanceAccountsQueryOptions().queryKey,
         (oldTotalBalance) => {
           const newBalance =
             Number(oldTotalBalance?.totalBalance) -
