6033675c4feca8387292eb18e8f0e959143f7fcb
afpedreros <felipe.pedreros94@gmail.com>
2024-10-25 16:12:52 -0500

    refactor(components): remove unused code and update styling in CategoryTag component

--- src/components/category-tag.tsx
+++ src/components/category-tag.tsx
@@ -1,6 +1,5 @@
 import { Icon } from "@iconify/react";
 import { Chip } from "@nextui-org/chip";
-import { cn } from "@nextui-org/theme";
 
 type CategoryTagProps = {
   name: string;
@@ -19,23 +18,14 @@ export function CategoryTag({
     <Chip
       classNames={{
         content: "!text-small px-1",
+        base: type === "expense" ? "bg-danger/20" : "bg-success/20",
       }}
-      endContent={
-        <span
-          className={cn(
-            "size-1.5 rounded-full",
-            type === "expense" ? "bg-danger" : "bg-success",
-          )}
-        />
-      }
       radius="sm"
       startContent={<Icon className="size-4" icon={icon} />}
       style={{
-        backgroundColor: `${color}15`,
         color: color,
-        border: `2px solid ${color}30`,
       }}
-      variant="flat"
+      variant="light"
     >
       {name}
     </Chip>
