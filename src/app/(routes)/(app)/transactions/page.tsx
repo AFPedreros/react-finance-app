4cf66393a1f0e6888a48cebb8129c59d540f5846
afpedreros <felipe.pedreros94@gmail.com>
2024-11-01 19:09:52 -0500

    refactor(pages): update heading styles and layout in Accounts, Categories, and Transactions pages

--- src/app/(routes)/(app)/transactions/page.tsx
+++ src/app/(routes)/(app)/transactions/page.tsx
@@ -1,7 +1,18 @@
 export default function TransactionsPage() {
   return (
-    <main className="h-screen p-6">
-      <h1>Transactions</h1>
+    <main className="flex h-[calc(100vh-64px)] flex-col gap-y-6 p-6">
+      <div className="flex items-center justify-between">
+        <div className="flex items-center gap-2">
+          <h1 className="inline w-fit bg-gradient-to-b from-[#5EA2EF] to-[#0072F5] bg-clip-text text-4xl font-bold leading-normal tracking-tight text-transparent lg:text-3xl">
+            Transactions
+          </h1>
+          {/* <TotalBalanceChip /> */}
+        </div>
+
+        {/* <CreateAccountDrawer /> */}
+      </div>
+
+      {/* <AccountsCard /> */}
     </main>
   );
 }
